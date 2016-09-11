1010a3604ae88e70dc732515dc0534d1a4b40e21
WinterThane <demonsaso@gmail.com>
2016-09-11 14:03:53 +0200

    Basic Grid
    Done basic grid with numbers.

---
+++ HexGrid/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+    public Text cellLabelPrefab;
+
+    private Canvas gridCanvas;
+    HexCell[] _cells;
+    
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        _cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        var cell = _cells[i] = Instantiate(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+
+        var label = Instantiate(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = x + "\n" + z;
+    }
+}
